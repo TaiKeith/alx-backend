711f5fc498446ff4bd5f062f4e4241fa4bd8f97d
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-30 16:38:23 +0300

    Created a class LRUCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This module contains a class LRUCache that inherits from BaseCaching and
+is a caching system.
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    A caching system that inherits from BaseCaching
+    """
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.order = []  # To track order of key accesses for LRU removal algo
+
+    def put(self, key, item):
+        """
+        Adds an item in the cache
+
+        Args:
+            key: Key to be used as an index
+            item: Item to be stored
+        Returns:
+            nothing if the key and item is none
+        """
+        if key is not None and item is not None:
+            # If key exists, remove it from order to update its position
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Remove the least recently used item
+                lru_key = self.order.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD:", lru_key)
+
+            # Add the new key and item, then update order list
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """Gets an item from the cache by key, updating order for LRU"""
+        if key in self.cache_data:
+            # Move accessed key to the end of the order list
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
