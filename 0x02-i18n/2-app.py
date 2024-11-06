b0b2cce74e8245e17e42bd5a6eef316b8b32ebcf
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-07 00:26:06 +0300

    Created a get_locale function with the babel.localeselector decorator in a Flask-Babel app setup

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     """
     Use request.accept_languages to determine the best match with supported
